71c05a716c2d9d0c6f850dc456f0fb7c9bb3ddd7
Boolie Kets <boolie@codingwithboolie.com>
2026-03-25 20:29:20 -0400

    Initial commit starting working on building tables

--- Tutorials/ConsoleApps/BasicCSharp/BasicCSharp/Program.cs
+++ Tutorials/ConsoleApps/BasicCSharp/BasicCSharp/Program.cs
@@ -1,14 +1,61 @@
 ﻿using System;
 
-class Program
+namespace LearningCodingInCSharp
 {
-    static void Main()
+    class MyProgram
     {
-        int score = 100; // Declare a variable
-        Console.WriteLine("Player's score is: " + score);
+        static void Main()
+        {
+            /*int score = 100; // Declare a variable
+            Console.WriteLine("Player's score is: " + score);
 
-        Console.WriteLine("What's your name?");
-        string firstName = Console.ReadLine() ?? "there";
-        Console.WriteLine("Hi, " + firstName + "! Welcome to C# programming!!");
+            Console.WriteLine("What's your name?");
+            string firstName = Console.ReadLine() ?? "there";
+            Console.WriteLine("Hi, " + firstName + "! Welcome to C# programming!!");*/
+
+            // Do while
+            /*int counter = 100;
+
+            do
+            {
+                Console.WriteLine("The counter is {0}", counter);
+                counter++;
+            } while (counter < 0); */
+
+            //while
+
+            /*int counter = 5;
+
+            while (counter > 0)
+            {
+                Console.WriteLine("The counter is {0}.", counter);
+                counter = counter - 1;
+            }*/
+
+            /*//Break
+            int i = 0;
+
+            for (i =0; i < 5; i++)
+            {
+                Console.WriteLine("i = {0}", i);
+                if (i == 2)
+                    break;
+            }*/
+
+
+        }
     }
+
+    class LearningCreatingClass
+    {
+        public static string greetings = "Bonsoir!";
+        public static int Age { get; set; }
+
+        public static void DisplayAge()
+        {
+            Age = 45;
+               Console.WriteLine("J'ai {0} ans. ", Age);
+        }
+    }
+
 }
---
+++ Tutorials/ConsoleApps/RomanNumeralToInteger/RomanNumeralToInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿public class RomanNumeralToInteger ()
+{
+    /*
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+     
+Constraints:
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+     
+     */
+}
