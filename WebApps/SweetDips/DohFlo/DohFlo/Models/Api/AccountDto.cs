9cf81fc7529ec2dddee736720c05092a6331e58f
Boolie Kets <boolie@codingcascade.com>
2026-09-24 21:05:45 -0400

    Added Account Api.

---
+++ WebApps/SweetDips/DohFlo/DohFlo/Models/Api/AccountDto.cs
@@ -0,0 +1,4 @@
+﻿namespace DohFlo.Models.Api
+{
+    public sealed record AccountDto(int Id, string Name, string Type, string Institution, string CurrencyCode, bool IsClosed);
+}
---
+++ WebApps/SweetDips/DohFlo/DohFlo/Models/Api/AccountWriteRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DohFlo.Models.Api
+{
+    public sealed class AccountWriteRequest
+    {
+        [Required(ErrorMessage = "Please enter an account name.")]
+        [StringLength(120)]
+        public string Name { get; set; } = "";
+
+        [Required(ErrorMessage = "Please select an account type.")]
+        [RegularExpression("^(Checking|Savings|Credit Card|Cash|Investment|Loan|Other)$",
+            ErrorMessage = "Please select a valid account type.")]
+        public string Type { get; set; }
+
+        [StringLength(200)]
+        public string? Institution { get; set; }
+
+        [Required]
+        [RegularExpression("^[A-Za-z]{3}$",
+            ErrorMessage = "Use a three-letter currency code, such as a USD.")]
+        public string CurrencyCode { get; set; }
+    }
+}
